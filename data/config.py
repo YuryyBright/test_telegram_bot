62827ca292441f459ca0b5f56cb67e4a817965a5
Ostrovskiy Yryy <yura.ostrovskiy.02@gmail.com>
2021-05-22 12:56:40 +0300

    start_upp

--- data/config.py
+++ data/config.py
@@ -4,6 +4,6 @@ from environs import Env
 env = Env()
 env.read_env()
 
-BOT_TOKEN = env.str("BOT_TOKEN")  # Забираем значение типа str
-ADMINS = env.list("ADMINS")  # Тут у нас будет список из админов
-IP = env.str("ip")  # Тоже str, но для айпи адреса хоста
+BOT_TOKEN = env.str("1839637938:AAF98ldmpcUlP14K3pVz2FJCDEOLpcrkkds")  # Забираем значение типа str
+ADMINS = env.list("412034942")  # Тут у нас будет список из админов
+IP = env.str("192.168.43.118")  # Тоже str", но для айпи адреса хоста
